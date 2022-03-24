62aa90d98326b24af5aa76a3efcf3f183810ce4b
Jeff Keddy <jeff.keddy@gmail.com>
2022-03-24 16:45:24 -0500

    init dom, scss and basic vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,128 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import ProductListing from './components/productListing.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      productName: 'Name'
+    }
+  },
+  methods: {
+    carouselCycle() {
+      
+    }
+  },
+  mounted() {
+    console.log(`The initial count is ${this.count}.`)
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template lang="pug">
+section.product-listing.js-product-listing
+  div.product-listing__media
+      div.product-carousel
+          div.product-carousel__wrapper
+              - for (var x = 0; x < 3; x++)
+                  img.product-carousel__image.product-carousel__image--hero(src='https://via.placeholder.com/700x700')
+              btn.product-carousel__btn.product-carousel__btn--arrow.product-carousel__btn--prev.js-product-carousel-prev Prev
+              btn.product-carousel__btn.product-carousel__btn--arrow.product-carousel__btn--next.js-product-carousel-next Next
+              btn.product-carousel__btn.product-carousel__btn--enlarge.js-product-carousel-enlarge Enlarge
+          div.product-carousel__thumbnails
+              img.product-carousel__image.product-carousel__image--thumbnail(src='https://via.placeholder.com/700x700')
+  div.product-listing__info
+      h2.product-listing__heading Heading
+      p.product-listing__model Model Number: 
+          strong 123456
+      p.product-listing__description Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec viverra dui. Nunc vel imperdiet ex. Quisque ut dapibus massa. Pellentesque sollicitudin ipsum et elit iaculis, ultricies egestas quam gravida. Sed quis enim leo. Aliquam iaculis sollicitudin ipsum, ut vestibulum arcu tristique id. Praesent pretium eros leo.
+      div.product-listing__options
+          p.product-listing__option-name Color/Finish 
+              strong Black
+          span.product-listing__option
+          p.product-listing__option-name Size/Width 
+              strong 32
+          span.product-listing__option
+          p.product-listing__option-name Some Third Thing 
+              strong 7
+          span.product-listing__option
+    
+</template>
+<style lang="scss">
+.product-listing {
+    display: flex;
+    align-items: flex-start;
+    &__media {
+        flex: 1;
+    }
+    &__info {
+        flex: 1;
+    }
+    &__heading {
+
+    }
+    &__model {
+
+    }
+    &__description {
+
+    }
+    &__options {
+
+    }
+    &__option {
+        // fix this
+        &-name{
+
+        }
+    }
+
+}
+.product-carousel {
+    max-width: 700px;
+    &__wrapper {
+        max-height: 700px;
+        overflow: hidden;
+        position: relative;
+    }
+    &__image {
+        margin: 0;
+        max-width: 100%;
+        z-index: 10;
+        &--hero {
+
+        }
+        &--thumbnail {
+            max-width: 50px;
+        }
+    }
+    &__btn {
+        background: rgba(0,0,0,.25);
+        color: #fff;
+        display: block;
+        height: 50px;
+        position: absolute;
+        width: 50px;
+        z-index: 20;
+        &--arrow {
+            margin-top: auto;
+            margin-bottom: auto;
+            top: 0;
+            bottom: 0;
+        }
+        &--prev {
+            left: 0;
+        }
+        &--next {
+            right: 0;
+        }
+        &--enlarge {
+            background: none;
+            bottom: 5%;
+            right: 5%;
+        }
+    }
+    &__thumbnails {
+
+    }
 }
 </style>
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// webpack.config.js
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin!
+    new VueLoaderPlugin()
+  ]
+}
