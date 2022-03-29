7ab6df0295e9e8dee0908c1bffefc41ac3296f73
Jeff Keddy <jeff.keddy@gmail.com>
2022-03-29 09:54:39 -0500

    added swapping products

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,10 @@ export default {
     },
     data() {
         return {
+            currentProduct: 0,
             products: [
                 {
+                    productType: 'Type1',
                     productName: "Name1",
                     productModel: "123",
                     productDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec viverra dui. Nunc vel imperdiet ex. Quisque ut dapibus massa. Pellentesque sollicitudin ipsum et elit iaculis, ultricies egestas quam gravida. Sed quis enim leo. Aliquam iaculis sollicitudin ipsum, ut vestibulum arcu tristique id. Praesent pretium eros leo.",
@@ -21,9 +23,10 @@ export default {
                     ]
                 },
                 {
+                    productType: 'Type2',
                     productName: "Name2",
-                    productModel: "123",
-                    productDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec viverra dui. Nunc vel imperdiet ex. Quisque ut dapibus massa. Pellentesque sollicitudin ipsum et elit iaculis, ultricies egestas quam gravida. Sed quis enim leo. Aliquam iaculis sollicitudin ipsum, ut vestibulum arcu tristique id. Praesent pretium eros leo.",
+                    productModel: "456",
+                    productDesc: "1111Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec viverra dui. Nunc vel imperdiet ex. Quisque ut dapibus massa. Pellentesque sollicitudin ipsum et elit iaculis, ultricies egestas quam gravida. Sed quis enim leo. Aliquam iaculis sollicitudin ipsum, ut vestibulum arcu tristique id. Praesent pretium eros leo.",
                     productImage: [
                         "https://via.placeholder.com/700x700/0000FF/808080?text=product+photo3",
                         "https://via.placeholder.com/700x700/0000FF/808080?text=product+photo4"
@@ -40,7 +43,9 @@ export default {
         };
     },
     methods: {
-        
+        changeProduct(val) {
+            this.currentProduct = val
+        }
     },
     beforeCreate() {
     },
@@ -60,15 +65,20 @@ export default {
 <template lang="pug">
 section.product-listing.js-product-listing
   div.product-listing__media
-      <ProductGallery v-bind:productImage="products[0].productImage"/>
-  div.product-listing__info(v-for="product in products")
-      h2.product-listing__heading {{product.productName}}
+      <ProductGallery v-bind:productImage="products[currentProduct].productImage"/>
+  div.product-listing__info
+      h2.product-listing__heading {{products[currentProduct].productName}}
       p.product-listing__model Model Number: 
-          strong {{product.productModel}}
-      p.product-listing__description {{product.productDesc}}
+          strong {{products[currentProduct].productModel}}
+      p.product-listing__description {{products[currentProduct].productDesc}}
       div.product-options
           p.product-options__name Color/Finish 
-              strong Black
+                strong {{products[currentProduct].productType}} 
+                <br>
+                btn.product-options__type(v-for="(product,index) in products"
+                  @click="changeProduct(`${index}`)"
+                  v-bind:data-product-type="`${index}`"
+                )
           span.product-listing__option
           p.product-options__name Size/Width 
               strong 32
@@ -109,6 +119,15 @@ section.product-listing.js-product-listing
     &__option{
 
     }
+    &__type {
+        background-color: black;
+        display: inline-block;
+        margin-right: 10px;
+        height: 20px;
+        width: 20px;
+    }
 }
-
+.visually-hidden {
+    display: none;
+}
 </style>
