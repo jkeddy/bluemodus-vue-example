3ee5417251f43bf7f75ec0118f10198f4505fbcd
Jeff Keddy <jeff.keddy@gmail.com>
2022-03-29 11:36:38 -0500

    gallery modal

--- src/components/productGallery.vue
+++ src/components/productGallery.vue
@@ -1,10 +1,12 @@
 <template lang="pug">
-div.product-gallery
+div.product-gallery(
+        :class="{'product-gallery--modal': isModalActive}"
+    )
     div.product-carousel
         img.product-gallery__image.product-gallery__image--hero(
             v-for="(imageUrl,index) in productImage"
             v-bind:src="imageUrl"
-            :id="`s${index+1}`"
+            :id="`s${index}`"
             :class="{'product-gallery__image--active': index === 0}"
         )
     btn.product-carousel__btn.product-carousel__btn--arrow.product-carousel__btn--prev.js-product-carousel-prev(
@@ -15,11 +17,18 @@ div.product-gallery
     ) Next
     btn.product-carousel__btn.product-carousel__btn--enlarge.js-product-carousel-enlarge(
         @click="galleryModal()"
+        :class="{'visually-hidden': isModalActive}"
     ) Enlarge
+    btn.product-carousel__btn.product-carousel__btn--enlarge.js-product-carousel-enlarge(
+        v-if="isModalActive"
+        @click="galleryModal()"
+    ) Close
     div.product-gallery__thumbnails
     img.product-gallery__image.product-gallery__image--thumbnail(
         v-for="imageUrl in productImage"
+        @click="carouselVal(`${index}`)"
         v-bind:src="imageUrl"
+        :id="`t${index}`"
     )
 </template>
 <script>
@@ -29,7 +38,24 @@ export default {
     data() {
         return {
             currentSlide: 1,
-            totalSlides: this.productImage.length
+            totalSlides: this.productImage.length,
+            isModalActive: false
+        }
+    },
+    computed: {
+        activeSlides() {
+            const activeSlides = [];
+            return activeSlides;
+        },
+        activeThumbnail() {
+            const activeThumb = ''
+            return activeThumb
+        },
+        isActive() {
+            return this.activeSlides.indexOf(this._uid) >= 0;
+        },
+        thumbnailMax() {
+            return this.totalSlides > 6
         }
     },
     props: {
@@ -37,7 +63,9 @@ export default {
     },
     methods: {
         carouselCycle(dir) {
+            // move carousel in direction provided. highlight thumbnail
             if(dir === 'next'){
+                //const activeSlide 
                 this.currentSlide++
                 console.log('next')
             }
@@ -47,8 +75,12 @@ export default {
             }
             console.log(this.currentSlide)
         },
+        carouselIndex(val) {
+            // take value change carousel position add highlight
+            console.log(val)
+        },
         galleryModal() {
-            alert("clicked");
+            this.isModalActive = !this.isModalActive
         }
     }
 }
@@ -72,6 +104,14 @@ export default {
         &--thumbnail {
             max-width: 50px;
         }
+    }
+    &--modal {
+        position: fixed;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        margin: auto;
     }
     &__thumbnails {
 
@@ -104,6 +144,7 @@ export default {
             right: 0;
         }
         &--enlarge {
+            color: black;
             background: none;
             bottom: 5%;
             right: 5%;
